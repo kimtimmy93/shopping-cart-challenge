a28c3388b3a09b4de567e7e6f00e2cbeb9947ba4
Tim Kim <tim@sdg.la>
2021-03-29 21:02:28 -0700

    Add Cart Component / Add Add to Cart to Home

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <template>
   <div id="nav">
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link
+      :class="{ active: $route.name === 'Cart' }"
+      to="/cart"
+    >
+      Cart
+    </router-link>
   </div>
   <router-view/>
 </template>
@@ -28,3 +33,10 @@
   }
 }
 </style>
+
+<script>
+export default {
+
+}
+</script>
+
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,25 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="ProductItem">
+    <button type="button" class="ProductItem__AddToCart">
+      Add To Cart
+    </button>
+  </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
-  }
+
+  },
 }
 </script>
+
+<style lang="scss">
+  .ProductItem {
+
+  }
+</style>
